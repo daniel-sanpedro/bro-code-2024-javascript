1057d90eb5055186e59ea6b3ac2c16e45a996298
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-11 15:17:22 -0700

    <added work>

--- variables/index.js
+++ variables/index.js
@@ -2,16 +2,44 @@
 //              Behaves as if it were a the value it contains.
 
 //  1. declaration      let x;
-//  2. assignment       x = 100; 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+//  2. assignment       x = 100;
+
+// let x;
+// x = 100;
+
+// console.log(x)
+
+// let x = 100; Both declarations and assignment
+
+// let age = 36;
+// console.log(age)
+
+// let price = 10.99;
+// console.log(price)
+
+// let gpa = 2.1;
+// console.log(gpa)
+
+// console.log(`you are ${age} years old, the price is $${price}  your gpa is:  ${gpa}`)
+
+
+// "" or '' makes it a string
+// let firstName = 'Daniel'
+// let favoriteFood = 'pizza'
+
+// console.log(typeof firstName)
+// console.log(`Your name is ${firstName}`)
+// console.log(`you like ${favoriteFood}`)
+// let email = 'daniel123@email.com'
+
+
+// boolean variables are true or false
+let online = false;
+let forSales = true;
+let isStudent = true;
+
+
+// should show as boolean
+console.log(typeof online)
+
+
