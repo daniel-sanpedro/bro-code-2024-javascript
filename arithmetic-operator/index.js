fe8eb4bb98b19e6883a534524cf66a97d6b7d787
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 11:51:31 -0700

    <completed arithmetic operator>

--- arithmetic-operator/index.js
+++ arithmetic-operator/index.js
@@ -2,4 +2,74 @@
 //                        operators (+ - * /)
 //                        ex. 11 = x + 5;
 
+// i am a teacher and have a group of students
+// let students = 30;
 
+// if a new student joins the class
+// students = students + 1;
+
+// if a student leaves the class
+// students = students - 1;
+
+// exponents is represented as **
+// students = students ** 2;
+
+// modulus operator gives you the remainder of any division
+// students = students % 2;
+// let extraStudents = students % 3;
+
+// console.log(ExtraStudents);
+
+// augmented assignment operators
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// increment operator
+// students++;
+
+// decrement operator
+// students--;
+
+// console.log(students);
+
+// operator precedence
+// 1. parenthesis ()
+// 2. exponents
+// 3. multiplication & division & modulo
+// 4. addition & subtraction
+
+// let result = 1 + 2 * 3 + 4 **2;
+
+// first solve exponents
+// 4 ** 2 = 16
+// = 1 + 2 * 3 + 16;
+// solve multiplication
+// 2 * 3 = 6
+// = 1 + 6 + 16;
+// solve addition
+// 1 + 6 + 16 = 23
+// 23
+
+// let result = 12 % 5 + 8 / 2;
+// solve modulo
+//  12 % 5 = 2
+// solve division
+// 8 / 2 = 4
+// solve addition
+// 2 + 4 = 6
+// 6
+
+// let result = 6 / 2 ** (2 + 5);
+// solve parenthesis
+// 2 + 5 = 7
+// solve exponents
+// 2 ** 7 = 128
+// division
+// 6 / 128 = 0.046875
+
+// console.log(result);
