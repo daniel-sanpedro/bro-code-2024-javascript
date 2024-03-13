b539af55f4d2dc25014d954f65a72b9848343952
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-13 12:26:42 -0700

    <accept user input completed>

---
+++ accept-user-input/README.md
@@ -0,0 +1,14 @@
+# Accept User Input
+
+## Description
+
+How to accept a user input
+
+## Technology
+
+-HTML
+-JavaScript
+
+## Author
+
+Daniel San Pedro
---
+++ accept-user-input/index.js
@@ -0,0 +1,21 @@
+// how to accept user input
+
+// 1. easy way = windows prompt
+// 2. professional way = HTML textbox
+
+// easy way
+// declare all variables to be used
+
+// let username;
+
+// username = window.prompt("whats your username?")
+
+// console.log(username)
+
+// professional way
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    // console.log(username)
+    document.getElementById("myH1").textContent = `hello ${username}`;
+}
