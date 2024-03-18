db2257e00066b47def38b455d988e69c9f888a96
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-18 00:32:37 -0700

    <completed css>

--- random-number-generator/index.js
+++ random-number-generator/index.js
@@ -1,1 +1,22 @@
 // Random number generator
+
+// let randomNum = Math.floor(Math.random() * 6) + 1;
+
+// console.log(randomNum);
+
+// const min = 50;
+// const max = 100;
+
+//                                         (max - min) to force operator precedence
+// let randomNum = Math.floor(Math.random() * (max - min)) + min;
+
+// console.log(randomNum);
+
+
+
+
+
+
+
+
+
