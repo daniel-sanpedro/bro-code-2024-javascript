f2fb158b8d884eabf85972ccd30cffbc5b7ef3cd
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-18 11:54:25 -0700

    <completed random number generator>

--- random-number-generator/index.js
+++ random-number-generator/index.js
@@ -12,11 +12,13 @@
 
 // console.log(randomNum);
 
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
 
-
-
-
-
-
-
-
+myButton.onclick = function () {
+  randomNum = Math.floor(Math.random() * max) + min;
+  myLabel.textContent = randomNum;
+};
