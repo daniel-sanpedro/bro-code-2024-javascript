c32f1c5a27bc5eda89e14025ebe8e2d8d054af2f
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-19 22:24:16 -0700

    <completed js>

--- checked-property/index.js
+++ checked-property/index.js
@@ -1,135 +1,29 @@
 // CHECKED PROPERTY
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// .checked = property that determines the checked state of an
+//            HTML checkbox or radio button element
+
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function () {
+  if (myCheckBox.checked) {
+    subResult.textContent = "you are Subscribed";
+  } else {
+    subResult.textContent = "you are not Subscribed";
+  }
+  if (visaBtn.checked) {
+    paymentResult.textContent = "you are paying with Visa";
+  } else if (masterCardBtn.checked) {
+    paymentResult.textContent = "you are paying with MasterCard";
+  } else if (payPalBtn.checked) {
+    payPalBtn.textContent = "you are paying with PayPal";
+  } else {
+    paymentResult.textContent = "you are not paying";
+  }
+};
