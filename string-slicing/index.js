2d7792129d0461ab6dbf8fa79c30cdcda017cee2
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-21 14:08:06 -0700

    <string slicing in progress>

--- string-slicing/index.js
+++ string-slicing/index.js
@@ -1,3 +1,78 @@
 // STRING SLICING
 
+// string slicing = creating a substring
+//                  from a portion of another string
+
+//                  string.slice(start, end)
+
+// const fullName = "daniel san pedro";
+
+// let firstName = fullName.slice(0, 6);
+
+// console.log(firstName)
+
+// results as follows:
+// daniel
+
+// const fullName = "daniel san pedro";
+
+// let firstName = fullName.slice(0, 6);
+// let lastName = fullName.slice(7, 16);
+
+// console.log(firstName)
+// console.log(lastName)
+
+// results as follows:
+// daniel 
+// san pedro
+
+// const fullName = "daniel san pedro";
+
+// let firstName = fullName.slice(0, 6);
+// let lastName = fullName.slice(7, 16);
+
+// let firstChar = firstName.slice(0, 1);
+// by using a negative number you start at the end
+// let lastChar = lastName.slice(-5)
+
+// console.log(firstChar, lastChar)
+
+// results as follows:
+// d pedro
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
