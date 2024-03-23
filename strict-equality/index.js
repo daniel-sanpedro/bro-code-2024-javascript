c4ebd555967873426fae96194441eac24fcbafb2
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-22 23:52:06 -0700

    <completed strict equality>

--- strict-equality/index.js
+++ strict-equality/index.js
@@ -1,3 +1,90 @@
 // STRICT EQUALITY
 
+// const PI = 3.14;
 
+// in this example pi has a datatype of number
+
+// console.log(typeof PI)
+
+//
+// == comparison operator
+//
+// if (PI == "3.14"){
+//     console.log("that is PI")
+// }
+// else {
+//     console.log("that is not PI")
+// }
+
+// results are as follows:
+// that is PI
+
+//
+// === strict equality operator
+//
+
+// if (PI === "3.14") {
+//   console.log("that is PI");
+// } else {
+//   console.log("that is not PI");
+// }
+
+// results are as follows:
+// this is not PI
+// reason is === compares both values and datatype
+//      PI is 3.14 number vs "3.14" is a string
+
+// if (PI == 3.14){
+//     console.log("that is PI")
+// }
+// else {
+//     console.log("that is not PI")
+// }
+
+// results are as follows:
+// this is PI
+
+//
+// != inequality operator
+//
+
+// is PI not equal to "3.14" the string
+// if (PI != "3.14"){
+//     console.log("that is not PI")
+// }
+// else {
+//     console.log("that is PI")
+// }
+
+// results are as follows:
+// that is PI
+// using the inequality operator != these values are still the same
+
+//
+// !== strict inequality operator
+//
+
+// are he values or datatype different
+// if (PI !== "3.14"){
+//     console.log("that is not PI")
+// }
+// else {
+//     console.log("that is PI")
+// }
+
+// results are as follows:
+// that is not PI
+// not the same datatype
+
+const PI = "3.14";
+
+// if (PI !== "3.14"){
+//     console.log("that is not PI")
+// }
+// else {
+//     console.log("that is PI")
+// }
+
+// results are as follows:
+// that is  PI
+// everything matched
