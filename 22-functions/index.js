9fb47b626a9ea2b4b6e4cc2016795a65d8dd392d
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-24 10:27:09 -0700

    <functions done>

--- 22-functions/index.js
+++ 22-functions/index.js
@@ -9,35 +9,70 @@
 
 // happyBirthday();
 
-function happyBirthday(username, age){
-    console.log("Happy Birthday!");
-    console.log("Happy Birthday!");
-    console.log(`Happy Birthday to you! ${username}`);
-    console.log("Happy Birthday!");
-    console.log(`you are ${age} years old`);
-}
+// function happyBirthday(username, age){
+//     console.log("Happy Birthday!");
+//     console.log("Happy Birthday!");
+//     console.log(`Happy Birthday to you! ${username}`);
+//     console.log("Happy Birthday!");
+//     console.log(`you are ${age} years old`);
+// }
+// happyBirthday("daniel", 36);
 
-happyBirthday("daniel", 36);
+// function add(x, y){
+//     let result = x + y;
+// return result;
+// }
 
+// let answer = add(2, 3);
+// console.log(answer);
 
+// function add(x, y) {
+//   return x + y;
+// }
 
+// console.log(add(2, 3));
 
+// function add(x, y) {
+//   return x + y;
+// }
+// function subtract(x, y) {
+//   return x - y;
+// }
+// function multiply(x, y) {
+//   return x * y;
+// }
+// function divide(x, y) {
+//   return x / y;
+// } function isEven(number){
+//     if (number % 2 === 0) {
+//       return true;
+//     } else {
+//       return false;
+//     }
 
+// }
 
+// console.log(isEven(10));
 
+//  function isEven(number){
+//  return number % 2 === 0 ? true : false;
 
+// }
 
+// console.log(isEven(10));
 
+// function isEven(number) {
+//   return number % 2 === 0 ? true : false;
+// }
+// function isValidEmail(email) {
+  // if(email.includes('@')){
+  //     return true;
+  // }
+  // else{
+  //     return false;
 
+  // }
+//   return email.includes("@") ? true : false;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// console.log(isValidEmail("sadasd@yahoo.com"));
