69683045bebf0f4f72dd9c09812e1a2d7a8c607d
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-03-23 23:46:50 -0700

    <added functions>

---
+++ 22-functions/README.md
@@ -0,0 +1,17 @@
+# FUNCTIONS
+
+## Description
+
+    Function = A section of reusable code.
+               Declare code once, use it whenever you want.
+               Call the function to execute that code.
+
+## Technology
+
+-HTML
+-CSS
+-JavaScript
+
+## Author
+
+Daniel San Pedro
---
+++ 22-functions/index.js
@@ -0,0 +1,43 @@
+// FUNCTIONS
+
+// function happyBirthday(){
+//     console.log("Happy Birthday!");
+//     console.log("Happy Birthday!");
+//     console.log("Happy Birthday to you!");
+//     console.log("Happy Birthday!");
+// }
+
+// happyBirthday();
+
+function happyBirthday(username, age){
+    console.log("Happy Birthday!");
+    console.log("Happy Birthday!");
+    console.log(`Happy Birthday to you! ${username}`);
+    console.log("Happy Birthday!");
+    console.log(`you are ${age} years old`);
+}
+
+happyBirthday("daniel", 36);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
