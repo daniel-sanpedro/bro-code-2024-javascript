c9c99f41f68780e2f378f9382368298e892f7486
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-13 15:08:37 -0700

    <completed arrays>

--- 25-arrays/index.js
+++ 25-arrays/index.js
@@ -1,3 +1,105 @@
-// 
+// ARRAYS
 
+// let fruits = ["apple", "orange", "banana"];
 
+// console.log(fruits);
+
+// result as follows:
+
+// (3) ["apple", "orange", "banana"]
+
+// let fruits = ["apple", "orange", "banana"];
+
+// console.log(fruits[0]);
+// // apple
+
+// console.log(fruits[1]);
+// // orange
+
+// console.log(fruits[2]);
+// // banana
+
+// console.log(fruits[3]);
+// // undefined
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits[0] = "coconut";
+
+// console.log(fruits);
+
+// result as follows:
+// (3) ['coconut', 'orange', 'banana']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits[1] = "coconut";
+
+// console.log(fruits);
+
+// result as follows:
+// (3) ['apple', 'coconut', 'banana']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.push("coconut");
+
+// console.log(fruits);
+// (4) ['apple', 'orange', 'banana', 'coconut']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.pop();
+
+// console.log(fruits);
+// (2) ['apple', 'orange']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.unshift("mango");
+
+// console.log(fruits);
+// (4) ['mango', 'apple', 'orange', 'banana']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.unshift("mango");
+
+// console.log(fruits);
+
+// 0:"mango"
+// 1:"apple"
+// 2:"orange"
+// 3:"banana"
+
+// let fruits = ["apple", "orange", "banana"];
+
+// fruits.shift();
+
+// console.log(fruits);
+
+// (2) ['orange', 'banana']
+
+// let fruits = ["apple", "orange", "banana"];
+
+// let numOfFruits = fruits.length;
+
+// console.log(numOfFruits);
+// 3
+
+// let fruits = ["apple", "orange", "banana"];
+
+// let numOfFruits = fruits.length;
+// let index = fruits.indexOf("apple");
+
+// console.log(index);
+//  0
+
+// let fruits = ["apple", "orange", "banana"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i]);
+// }
+// apple
+// orange
+// banana
