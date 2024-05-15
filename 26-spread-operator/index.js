55faccacd262b42c1397d3748c1379daa5120974
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-14 23:50:58 -0700

    <spread operator complete>

--- 26-spread-operator/index.js
+++ 26-spread-operator/index.js
@@ -1,1 +1,10 @@
 // SPREAD OPERATOR
+
+// let numbers = [1, 2, 3, 4, 5];
+
+// console.log(numbers);
+
+let usernames = "Daniel San Pedro";
+let letters = [...usernames].join("-");
+
+console.log(letters);
