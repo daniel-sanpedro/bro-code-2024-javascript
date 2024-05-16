2bb1578dc3232c2644f5c1b23d8465dd2d139915
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-15 17:32:45 -0700

    <added random password>

--- 29-⭐random-password-generator/index.js
+++ 29-⭐random-password-generator/index.js
@@ -1,3 +1,7 @@
-// 
+// RANDOM PASSWORD GENERATOR
 
-
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
