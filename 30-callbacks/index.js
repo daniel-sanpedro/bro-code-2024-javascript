13d1638f77d8089e9549db8c3da11057ff164e86
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-16 10:43:28 -0700

    <close callback>

--- 30-callbacks/index.js
+++ 30-callbacks/index.js
@@ -1,1 +1,46 @@
 // CALLBACKS
+
+// hello();
+// goodbye();
+// function hello() {
+//   setTimeout(function () {
+//     console.log("hello");
+//   }, 3000);
+// }
+
+// function goodbye() {
+//   console.log("goodbye");
+// }
+
+// hello(goodbye);
+
+// function hello(callback) {
+//   console.log("hello");
+//   callback();
+// }
+
+// function wait() {
+//   console.log("wait");
+// }
+
+// function leave() {
+//   console.log("leave");
+// }
+
+// function goodbye() {
+//   console.log("goodbye");
+// }
+
+sum(displayPage, 1, 2);
+
+function sum(callback, x, y) {
+  let result = x + y;
+  callback(result);
+}
+function displayConsole(result) {
+  console.log(result);
+}
+
+function displayPage(result) {
+  document.getElementById("myH1").textContent = result;
+}
