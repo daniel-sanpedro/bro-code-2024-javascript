6d83ee69402e024e563604d73142f68143a5f1d7
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-19 13:55:15 -0700

    <added more info>

--- 37-javascript-objects/index.js
+++ 37-javascript-objects/index.js
@@ -5,6 +5,8 @@ const person1 = {
   lastName: "Squarepants",
   age: 30,
   isEmployed: true,
+  sayHello: function(){console.log("Hi, I am Spongebob!")};
+  sayBye: function(){console.log("Goodbye")};
 };
 
 const person2 = {
@@ -12,6 +14,20 @@ const person2 = {
   lastName: "Starfish",
   age: 42,
   isEmployed: false,
+  sayHello: () => console.log("Hi, I am Patrick!");
+  sayBye: function(){console.log("Goodbye")};
 };
 
-console.log(person1);
+console.log(person1.firstName);
+console.log(person1.lastName);
+console.log(person1.age);
+console.log(person1.isEmployed);
+console.log(person1.sayHello);
+console.log(person1.sayBye);
+
+console.log(person2.firstName);
+console.log(person2.lastName);
+console.log(person2.age);
+console.log(person2.isEmployed);
+console.log(person2.sayHello);
+console.log(person2.sayBye);
