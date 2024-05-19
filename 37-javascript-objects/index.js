632b9d248d92873d3642a7a4ad3d46e3b7f0b825
daniel-sanpedro <dpsanpedro@yahoo.com>
2024-05-18 22:54:53 -0700

    <added patrick>

---
+++ 37-javascript-objects/index.js
@@ -0,0 +1,17 @@
+// JAVASCRIPT OBJECTS
+
+const person1 = {
+  firstName: "Spongebob",
+  lastName: "Squarepants",
+  age: 30,
+  isEmployed: true,
+};
+
+const person2 = {
+  firstName: "Patrick",
+  lastName: "Starfish",
+  age: 42,
+  isEmployed: false,
+};
+
+console.log(person1);
